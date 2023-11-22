15ae25eaf6241de7d39a1c88ba38958e9df61fd5
Arun Kumar <aruncode49@gmail.com>
2023-11-22 15:52:11 +0530

    power of two

---
+++ 3. Binary and Decimal Number System/power_of_two.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+class Solution
+{
+public:
+
+// first method -> not optimize
+    bool first_method(int n)
+    {
+
+        for (int i = 0; i <= 30; i++)
+        {
+            int ans = pow(2, i);
+
+            if (ans == n)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+// second method -> optimized
+    bool second_method(int n)
+    {
+        int ans = 1;
+        for (int i = 0; i <= 30; i++)
+        {
+            if (ans == n)
+            {
+                return true;
+            }
+
+            if(ans < INT_MAX / 2)
+                ans = ans * 2;
+        }
+
+        return false;
+    }
+};
